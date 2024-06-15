9aceab75bc7042480dc36a6ea59b8b4ed9829c12
Julian Metzler <git@mezgr.de>
2024-06-15 11:48:52 +0200

    Update copyright years

--- util.cpp
+++ util.cpp
@@ -1,5 +1,5 @@
 /*
-Copyright 2023 Julian Metzler
+Copyright 2023-2024 Julian Metzler
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
